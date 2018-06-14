a7f17378201066975d76ad4c93eb10b1fb9fb30c
Ossaija ThankGod <codebugsolved@gmail.com>
2018-06-14 17:28:48 +0100

    fix(src/config/index.ts): resolves build not working on CI server
    resolves build failing on CI because of no PROCESS_TYPE environment variable

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ import dotenv from  "dotenv";
 import { Config } from "../contracts/config";
 dotenv.load();
 
-const processType = process.env.PROCESS_TYPE;
+const processType = process.env.PROCESS_TYPE || "web";
 let env: Config;
 try {
   env = require(`./${processType}`);
