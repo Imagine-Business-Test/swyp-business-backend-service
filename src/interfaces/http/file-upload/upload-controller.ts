e435686a895ab7b508293900aaff2bc02293d1cc
Ossaija ThankGod <codebugsolved@gmail.com>
2019-04-03 09:37:56 +0100

    fix: Trying to figure out while picture and video are not uploading on production
    Trying to figure out while picture and video are not uploading on production

--- src/interfaces/http/file-upload/upload-controller.ts
+++ src/interfaces/http/file-upload/upload-controller.ts
@@ -13,6 +13,7 @@ export const FileUploadController = {
   },
 
   uploadAsset(req: any, res: Response, next: any) {
+    console.log("request started");
     req.validateParams(FileUploadRule.newAsset);
     const handler = req.container.resolve("S3Uploader") as FileUploader;
     const path = `${req.params.bankname}/${req.params.assetType}`;
@@ -23,6 +24,7 @@ export const FileUploadController = {
         try {
           const singleUpload = uploader.single("asset");
           singleUpload(req, res, (err: Error) => {
+            console.log("making actual request");
             if (err) {
               return res
                 .status(422)
